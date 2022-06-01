fa5397ad92a110cd2ea32f1f93edc55408dbb9df
Umlatt <theplacetosendmemail@gmail.com>
2022-06-01 12:46:22 +0000

    Added public IP send

--- server_start.sh
+++ server_start.sh
@@ -9,5 +9,9 @@ trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>>$logfile 2>&1
 # Logging config end
 echo "##################### STARTING SERVER ######################"
+# Create a server.config file which contains relevant connection info for gamers (in the config directory)
+conf_file="/home/steam/config/server.config"
+public_ip=$(/usr/bin/dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | tr -d '"')
+printf "IP [$public_ip]" > $conf_file
 # CUSTOM GAME SERVER COMMAND
 ./data/srcds_run -game csgo -console -usercon +game_type 0 +game_mode 0 +mapgroup mg_active +map de_dust2 +sv_setsteamaccount $STEAM_APP_ID
